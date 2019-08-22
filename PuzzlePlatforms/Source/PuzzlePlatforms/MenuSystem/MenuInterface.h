20934bc8ec8a999d8fc55faf292782d5ce378e66
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-08-21 20:21:59 -0600

    Adds pure virtual function Host to Menu Interface

--- PuzzlePlatforms/Source/PuzzlePlatforms/MenuSystem/MenuInterface.h
+++ PuzzlePlatforms/Source/PuzzlePlatforms/MenuSystem/MenuInterface.h
@@ -22,4 +22,6 @@ class PUZZLEPLATFORMS_API IMenuInterface
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
+
+	virtual void Host() = 0;
 };
