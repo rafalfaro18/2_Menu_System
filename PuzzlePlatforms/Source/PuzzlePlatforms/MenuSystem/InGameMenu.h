925b0182f95180d5d961d70cd6a3af2cf15f0280
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-08-26 20:22:09 -0600

    Adds new User Widget class for In Game Menu

---
+++ PuzzlePlatforms/Source/PuzzlePlatforms/MenuSystem/InGameMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InGameMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API UInGameMenu : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
